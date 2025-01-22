b7d9ea0bbf71705470e69e270e003d648200e7e0
jayganjawala <jayganjawala.it22@scet.ac.in>
2025-01-22 08:01:21 +0000

    Commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import SignUpForm from './SignUpForm';
+import NoteApp from './NoteApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App/>
     <SignUpForm/>
+    <NoteApp/>
   </React.StrictMode>
 );
 
